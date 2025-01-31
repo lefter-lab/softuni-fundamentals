cd3f195def3864cd518e73cca8e9002241f95780
lefter-lab <leftergsm@gmail.com>
2025-01-31 22:07:16 +0200

    Update A2_CommonElements.java

--- A2_CommonElements.java
+++ A2_CommonElements.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class A2_CommonElements {
+    public static void main(String[] args) {
+        String[] arrayOne;
+        String lineTwo;
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            String lineOne = scanner.nextLine();
+            arrayOne = lineOne.split(" ");
+            lineTwo = scanner.nextLine();
+        }
+        String[] arrayTwo = lineTwo.split(" ");
+
+        for (int i = 0; i < arrayTwo.length; i++) {
+
+            for (int j = 0; j < arrayOne.length; j++) {
+
+                if(arrayTwo[i].equals(arrayOne[j])) {
+
+                    System.out.print(arrayOne[j] + " ");
+
+                }
+            }
+        }
+    }
 }
