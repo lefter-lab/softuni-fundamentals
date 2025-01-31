bf3722d8994f311b0269c376d4c8d6367893d381
TodorLeft <vdverkov40@gmail.com>
2025-01-31 22:16:00 +0200

    Update A1_Train.java
    add mesage

--- A1_Train.java
+++ A1_Train.java
@@ -24,6 +24,6 @@ public class A1_Train {
         System.out.println(sum);
     }
 }
-/*You will be given a count of wagons in a train n. On the next n lines,
+/*When You will be given a count of wagons in a train n. On the next n lines,
 you will receive how many people will get on that wagon. In the end,
 print the whole train and the sum of the people on the train*/
