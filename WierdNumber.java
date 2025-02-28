df45c06b1559c56d65c41bba3c5560d098c1d07e
lefter-lab <leftergsm@gmail.com>
2025-02-28 10:03:15 +0200

    add q25d

--- WierdNumber.java
+++ WierdNumber.java
@@ -9,7 +9,7 @@ public class WierdNumber {
         if (n % 2 != 0 || (n >= 6 && n <= 20)) {
             System.out.println("Weird1");
         } else {
-            System.out.println("Not Weirdq25 ае");
+            System.out.println("Not Weirerdq25 ае");
         }
     }
 }
