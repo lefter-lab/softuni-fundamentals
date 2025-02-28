9adba48033edf6d87949928aa04adad06fb1c3e8
lefter-lab <leftergsm@gmail.com>
2025-02-28 09:24:46 +0200

    hacher rank if steatment name

--- WierdNumber.java
+++ WierdNumber.java
@@ -6,16 +6,10 @@ public class WierdNumber {
         int n = scanner.nextInt();
         scanner.close();
 
-        if (n % 2 != 0) { // Check if n is odd
+        if (n % 2 != 0 || (n >= 6 && n <= 20)) {
             System.out.println("Weird");
         } else {
-            if (n >= 2 && n <= 5) {
-                System.out.println("Not Weird");
-            } else if (n >= 6 && n <= 20) {
-                System.out.println("Weird");
-            } else if (n > 20) {
-                System.out.println("Not Weird");
-            }
+            System.out.println("Not Weird");
         }
     }
 }
