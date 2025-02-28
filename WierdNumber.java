6829875ff5d33a4cf12887dde410b0da62e3d3fd
lefter-lab <leftergsm@gmail.com>
2025-02-28 09:10:30 +0200

    hacher rank if steatment name

--- WierdNumber.java
+++ WierdNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solution {
+public class WierdNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
